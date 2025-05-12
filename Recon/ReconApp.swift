ec985526ee88a48fdbc1ab7663478db23c81a389
Gracek <leemonka3@gmail.com>
2025-05-12 03:02:59 +0200

    Initial Commit

---
+++ Recon/ReconApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReconApp.swift
+//  Recon
+//
+//  Created by Gracek on 12/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
